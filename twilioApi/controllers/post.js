355badfb0272f7d6da1f3311cd0faf2a350fb808
Necoline <hnhubner@gmail.com>
2019-07-16 16:20:47 -0700

    adds .gitignore and dotenv

--- twilioApi/controllers/post.js
+++ twilioApi/controllers/post.js
@@ -1,101 +1,90 @@
-const Post =  require('../models/post');
+const Post = require("../models/post");
 
 /**
  * List Posts
  */
 exports.list = (req, h) => {
-  return Post.find({}).exec().then((post) => {
-
-    return { posts: post };
-
-  }).catch((err) => {
-
-    return { err: err };
-
-  });
-}
+  return Post.find({})
+    .exec()
+    .then(post => {
+      return { posts: post };
+    })
+    .catch(err => {
+      return { err: err };
+    });
+};
 
 /**
  * Get Post by ID
  */
 exports.get = (req, h) => {
+  return Post.findById(req.params.id)
+    .exec()
+    .then(post => {
+      if (!post) return { message: "post not Found" };
 
-  return Post.findById(req.params.id).exec().then((post) => {
-
-    if(!post) return { message: 'post not Found' };
-
-    return { post: post };
-
-  }).catch((err) => {
-
-    return { err: err };
-
-  });
-}
-
+      return { post: post };
+    })
+    .catch(err => {
+      return { err: err };
+    });
+};
 
 /**
  * POST a Post
  */
 exports.create = (req, h) => {
+  console.log("req", req.payload);
   const postData = {
     question: req.payload.question,
     voteCount: req.payload.voteCount || 0
   };
 
-  console.log('postData', postData )
+  return Post.create(postData)
+    .then(post => {
+      return { message: "Post created successfully", post: post };
+    })
+    .catch(err => {
+      console.log("error");
 
-  return Post.create(postData).then((post) => {
-    console.log('post', post)
-     return { message: "Post created successfully", post: post };
-
-  }).catch((err) => {
-    console.log('error')
-
-    return { err: err };
-
-  });
-}
+      return { err: err };
+    });
+};
 
 /**
  * PUT | Update Post by ID
  */
 exports.update = (req, h) => {
+  return Post.findById(req.params.id)
+    .exec()
+    .then(post => {
+      if (!post) return { err: "Post not found" };
 
-  return Post.findById(req.params.id).exec().then((post) => {
+      post.question = req.payload.question;
+      post.voteCount = req.payload.voteCount;
 
-    if (!post) return { err: 'Post not found' };
-
-    post.question = req.payload.question;
-    post.voteCount = req.payload.voteCount;
-
-    post.save(postData);
-
-  }).then((data) => {
-
+      post.save(postData);
+    })
+    .then(data => {
       return { message: "Post data updated successfully" };
-
-  }).catch((err) => {
-
+    })
+    .catch(err => {
       return { err: err };
-
-  });
-}
+    });
+};
 
 /**
  * Delete Post by ID
  */
 exports.remove = (req, h) => {
-
-  return Post.findById(req.params.id).exec(function (err, post) {
-
+  return Post.findById(req.params.id).exec(function(err, post) {
     if (err) return { dberror: err };
-    if (!post) return { message: 'Post not found' };
+    if (!post) return { message: "Post not found" };
 
-    post.remove(function (err) {
+    post.remove(function(err) {
       if (err) return { dberror: err };
 
       return { success: true };
     });
   });
-}
+};
